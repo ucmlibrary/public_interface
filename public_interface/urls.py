1f0e1be6b4ab69ace77a6a26046b32f3048a9602
amy wieliczka <amywieliczka@berkeley.edu>
2014-08-19 18:13:09 -0700

    Added list and thumbnail views, added object view, working on carousel

--- public_interface/urls.py
+++ public_interface/urls.py
@@ -7,7 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'solrapi.views.home', name='home'),
     url(r'^search/', 'solrapi.views.search', name='search'),
-    url(r'^objectView/$', 'solrapi.views.objectView', name='objectView'),
+    url(r'^objectView/', 'solrapi.views.objectView', name='objectViewPost'),
+    url(r'^objectView/(?P<object_id>.*)/', 'solrapi.views.objectView', name='objectView'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- solrapi/views.py
+++ solrapi/views.py
@@ -8,11 +8,28 @@ import solr
 
 FACET_TYPES = [('type', 'Type of Object'), ('repository_name', 'Institution Owner'), ('collection_name', 'Collection')]
 
+def solrize_query(q, rq):
+    if q == '':
+        return rq
+    elif rq == '':
+        return q
+    else:
+        return q + ' AND ' + rq
+
 def solrize_filters(filters, filter_type):
-    fq = []
-    for f in filters:
-        fq.append(filter_type + ': "' + f + '"')
-    return fq
+    # fq = []
+    # for f in filters:
+    #     fq.append(filter_type + ': "' + f + '"')
+    # print fq
+    
+    # TRIAL FOR OR-ING FILTERS OF ONE TYPE, YET AND-ING FILTERS OF DIFF TYPES
+    trial = filter_type + ': '
+    for counter, f in enumerate(filters):
+        trial = trial + '"' + f + '"'
+        if counter < len(filters)-1:
+            trial = trial + " OR "
+    
+    return [trial]
 
 def process_facets(facets, filters):
     display_facets = {}
@@ -25,22 +42,24 @@ def process_facets(facets, filters):
 
 def search(request):
     if request.method == 'GET':
-        
         # concatenate query and refine query form fields
         if len(request.GET.getlist('q')) > 1:
-            q = reduce(lambda x, y: x + " AND " + y, request.GET.getlist('q'))
+            q = reduce(solrize_query, request.GET.getlist('q'))
         else: 
             q = request.GET['q']
         
         rows = request.GET['rows'] if 'rows' in request.GET else '16'
         start = request.GET['start'] if 'start' in request.GET else '0'
+        view_format = request.GET['view_format'] if 'view_format' in request.GET else 'thumbnails'
         
         filters = dict((filter_type[0], request.GET.getlist(filter_type[0])) for filter_type in FACET_TYPES)
         # make filter form fields solr search friendly
         fq = []
         for filter_type in FACET_TYPES:
-            if filters[filter_type[0]] > 0:
+            if len(filters[filter_type[0]]) > 0:
                 fq.extend(solrize_filters(filters[filter_type[0]], filter_type[0]))
+        
+        print q
         
         # perform the search
         s = solr.Solr('http://107.21.228.130:8080/solr/dc-collection')
@@ -59,7 +78,7 @@ def search(request):
                 solr_response.facet_counts['facet_fields'][facet_type[0]], 
                 filters[facet_type[0]]
             )
-
+        
         return render(request, 'public_interface/searchResults.html', {
             'q': q,
             'filters': filters,
@@ -69,20 +88,44 @@ def search(request):
             'facets': facets,
             'FACET_TYPES': FACET_TYPES,
             'numFound': solr_response.numFound,
-            'pages': int(math.ceil(float(solr_response.numFound)/int(rows)))
+            'pages': int(math.ceil(float(solr_response.numFound)/int(rows))),
+            'view_format': view_format
         })
-    
+        
     return render (request, 'public_interface/base.html', {'q': ''})
 
 def home(request):
     return render(request, 'public_interface/base.html', {'q': ''})
 
-def objectView(request):
-    queryVar = "\"1916-preparedness-day-parade-bombing-1916-1933-pho--http://ark.cdlib.org/ark:/13030/tf0c6007xf\""
+def objectView(request, object_id=''):
+    s = solr.Solr('http://107.21.228.130:8080/solr/dc-collection')
     
-    s = solr.Solr('http://54.234.57.180:8080/solr/dc-collection')
-    rsp = s.select(q='id:' + queryVar, facet='true')
-    context = {'params': {'q': queryVar}, 'docs': rsp.results}
+    if request.method == 'POST':
+        object_id = 'id:' + "\"" + request.POST['object_id'] + "\""
+        
+        q = request.POST['q'] if 'q' in request.POST else ''
+        start = request.POST['start'] if 'start' in request.POST else '0'
+        
+        filters = dict((filter_type[0], request.POST.getlist(filter_type[0])) for filter_type in FACET_TYPES)
+        # make filter form fields solr search friendly
+        fq = []
+        for filter_type in FACET_TYPES:
+            if len(filters[filter_type[0]]) > 0:
+                fq.extend(solrize_filters(filters[filter_type[0]], filter_type[0]))
+        
+        solr_response = s.select(
+            q=q,
+            rows='6',
+            start=start,
+            fq=fq
+        )
+    else:
+        solr_response = {'results': ''}
+        q = object_id
+    
+    solr_object = s.select(q=object_id)
+    
+    context = {'q': q, 'docs': solr_object.results, 'carousel': solr_response.results, 'numFound': solr_response.numFound}
     
     return render(request, 'public_interface/object.html', context)
 
