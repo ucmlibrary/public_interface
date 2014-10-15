ec4c538a3fc4abeeab1f48b9971ad4407c30f896
JoelUCOP <joel.hagedorn@ucop.edu>
2014-10-15 16:40:56 -0700

    create test carousel using code examples

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+// Slick carousel
+
+$(document).ready(function(){
+	$('.carousel').slick({
+	  dots: true,
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 5,
+	  slidesToScroll: 5,
+	  responsive: [
+	    {
+	      breakpoint: 1200,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 4,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 992,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3
+	      }
+	    },
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    }
+	  ]
+	});
+});
