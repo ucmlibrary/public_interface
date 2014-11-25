f2f95f71d72aa768ee3a617c22f6d9e5bf3f9a23
JoelUCOP <joel.hagedorn@ucop.edu>
2014-11-25 15:14:28 -0800

    clean up main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,10 @@
 
 $(document).ready(function(){
 
-// ##### Popover ##### //
-
-  // $('.popover__link').popover({
-  //   trigger: 'hover',
-  //   placement: 'auto',
-  //   html: true
-  // });
-
-// if (Modernizr.mq('only screen and (max-width: 800px)')) {
-// 	$('.popover__link').popover('destroy');
-// }
-
-// ##### Global Header ##### //
-	
-	// Toggle mobile menu with search box:
-	$('.js-global-header__bars-icon').click(function(){
+  // ##### Global Header ##### //
+  	
+  // Toggle mobile menu with search box:
+  $('.js-global-header__bars-icon').click(function(){
     $('.js-global-header__search').toggleClass('global-header__search global-header__search--selected');
     $('.js-global-header__mobile-links').toggleClass('.global-header__mobile-links global-header__mobile-links--selected');
   });
@@ -27,10 +15,10 @@ $(document).ready(function(){
     $('.js-global-header__search').toggleClass('global-header__search global-header__search--selected');
   });
 
-// ##### Checkbox Groups ##### //
+  // ##### Checkbox Groups ##### //
 
-	// Expand checkbox group when clicking on header (small and medium screens):
-	$('.js-check__header').click(function(){
+  // Expand checkbox group when clicking on header (small and medium screens):
+  $('.js-check__header').click(function(){
     $('.js-check__popdown').toggleClass('check__popdown check__popdown--selected');
   });
 
@@ -82,48 +70,61 @@ $(document).ready(function(){
   	}
   });
 
+}); // End of $(document).ready(function()
+
+// ##### Popover ##### //
+
+$('.popover__link').popover({
+  trigger: 'hover',
+  placement: 'auto',
+  html: true
+});
+
+// Alternative JavaScript method (instead of CSS method) for disabling popover via breakpoint:
+// if (Modernizr.mq('only screen and (max-width: 800px)')) {
+//  $('.popover__link').popover('destroy');
+// }
+
 // ##### Carousel ##### //
-	
-	// Get Bootstrap CSS breakpoints;
-	var lg = $('.bs-lg-screen').css('width');
-	var md = $('.bs-md-screen').css('width');
-	var sm = $('.bs-sm-screen').css('width');
 
-	lg = lg.replace('px', '');
-	md = md.replace('px', '');
-	sm = sm.replace('px', '');
+// Get Bootstrap CSS breakpoints:
+var lg = $('.bs-lg-screen').css('width');
+var md = $('.bs-md-screen').css('width');
+var sm = $('.bs-sm-screen').css('width');
 
-	// To display Bootstrap breakpoint for testing, uncomment this and add <div id="test"></div> to HTML:
-	// document.getElementById('test').innerHTML = lg;
+lg = lg.replace('px', '');
+md = md.replace('px', '');
+sm = sm.replace('px', '');
 
-	// Slick settings:
-	$('.carousel').slick({
-	  infinite: false,
-	  slidesToShow: 5,
-	  slidesToScroll: 5,
-	  responsive: [
-	    {
-	      breakpoint: lg,
-	      settings: {
-	        slidesToShow: 4,
-	        slidesToScroll: 4,
-	      }
-	    },
-	    {
-	      breakpoint: md,
-	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3
-	      }
-	    },
-	    {
-	      breakpoint: sm,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2
-	      }
-	    }
-	  ]
-	});
+// To display a Bootstrap breakpoint value for testing, uncomment next line, set the breakpoint variable, then add <div id="bp-test"></div> to HTML:
+// document.getElementById('bp-test').innerHTML = lg;
 
-}); // End of (document).ready(function()
+// Slick settings (see http://kenwheeler.github.io/slick/):
+$('.carousel').slick({
+  infinite: false,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: lg, // Bootstrap breakpoint variable set at top of this file
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    },
+    {
+      breakpoint: md, // Bootstrap breakpoint variable set at top of this file
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: sm, // Bootstrap breakpoint variable set at top of this file
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
+});
