468902b6592f1a3921930480991aa520fd866223
JoelUCOP <joel.hagedorn@ucop.edu>
2014-12-05 16:00:44 -0800

    update 'copy' task src in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -321,7 +321,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/{,*/}*.*',
+            'fonts/{,*/}*.*'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
