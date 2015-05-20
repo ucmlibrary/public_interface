d5ebd4abeb33631f3fc1b7442fc0612b05a93b5d
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-05-19 22:32:34 -0700

    fill in links in header and footer

--- calisphere/urls.py
+++ calisphere/urls.py
@@ -4,18 +4,25 @@ urlpatterns = patterns('calisphere',
     url(r'^$', 'views.home', name='home'),
     url(r'^search/$', 'views.search', name='search'),
     url(r'^itemView/(?P<item_id>.*)/', 'views.itemView', name='itemView'),
-    
+
     url(r'^collections/$', 'views.collectionsDirectory', name='collectionsDirectory'),
     url(r'^collections/(?P<collection_letter>[a-zA-Z]{1})/', 'views.collectionsAZ', name='collectionsAZ'),
     url(r'^collections/(?P<collection_id>\d*)/', 'views.collectionView', name='collectionView'),
     url(r'^collections/titleSearch/$', 'views.collectionsSearch', name='collectionsTitleSearch'),
     url(r'^collections/', 'views.collectionsSearch', name='collectionsSearch'),
-    
+
     url(r'^institution/(?P<repository_id>.*)/', 'views.repositoryView', name='repositoryView'),
     url(r'^campus/(?P<campus_id>.*)/', 'views.campusView', name='campusView'),
     url(r'^institutions/uc-partners/$', 'views.campusDirectory', name='campusDirectory'),
     url(r'^institutions/statewide-partners/$', 'views.statewideDirectory', name='statewideDirectory'),
-    
+
+    url(r'about/$', 'views.home', name='about'),
+    url(r'contact/$', 'views.home', name='contact'),
+    url(r'help/$', 'views.home', name='help'),
+    url(r'terms/$', 'views.home', name='termsOfUse'),
+    url(r'privacy/$', 'views.home', name='privacyStatement'),
+    url(r'siteMap/$', 'views.home', name='siteMap'),
+
     # AJAX HELPERS
     url(r'^relatedCollections/', 'views.relatedCollections', name='relatedCollections'),
     url(r'^carousel/', 'views.itemViewCarousel', name='carousel'),
