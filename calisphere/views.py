a273ca3f48d16654050cc944c511492fa9393467
Amy Wieliczka <amywieliczka@gmail.com>
2015-08-19 18:00:21 -0700

    Getting closer to having more like this results

--- calisphere/views.py
+++ calisphere/views.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from django.http import Http404
 from calisphere.collection_data import CollectionManager
 from constants import *
-from cache_retry import SOLR_select, json_loads_url
+from cache_retry import SOLR_select, SOLR_raw, json_loads_url
 
 import operator
 import math
@@ -12,6 +12,7 @@ import re
 import copy
 import simplejson as json
 import string
+import solr
 
 # concat query with 'AND'
 def concat_query(q, rq):
@@ -422,17 +423,20 @@ def itemViewCarousel(request):
         fq.append('campus_url: "https://registry.cdlib.org/api/v1/campus/' + campus_id + '/"')
 
     mlt = False
-    if queryParams['q'] == '' and fq == '':
+    if queryParams['q'] == '' and len(fq) == 0:
         mlt=True,
         mlt_fl='title,collection_name,subject'
 
     if mlt:
-        carousel_solr_search = SOLR_select(
+        carousel_solr_search = SOLR_raw(
             q='id:'+item_id,
             fields='id, type_ss, reference_image_md5, title',
             mlt='true',
-            mlt_fl=['title', 'collection_name', 'subject']
+            mlt_count='12',
+            mlt_fl=mlt_fl
         )
+        search_results = json.loads(carousel_solr_search)['moreLikeThis'][item_id]['docs']
+        numFound = json.loads(carousel_solr_search)['moreLikeThis'][item_id]['numFound']
     else:
         carousel_solr_search = SOLR_select(
             q=queryParams['query_terms'],
@@ -441,13 +445,15 @@ def itemViewCarousel(request):
             start=queryParams['start'],
             fq=fq
         )
+        search_results = carousel_solr_search.results
+        numFound = carousel_solr_search.numFound
 
     if 'init' in request.GET:
         return render(request, 'calisphere/carouselContainer.html', {
             'q': queryParams['q'],
             'start': queryParams['start'],
-            'numFound': carousel_solr_search.numFound,
-            'search_results': carousel_solr_search.results,
+            'numFound': numFound,
+            'search_results': search_results,
             'item_id': item_id,
             'referral': request.GET['referral'] if 'referral' in request.GET else '',
             'referralName': request.GET['referralName'] if 'referralName' in request.GET else '',
@@ -456,7 +462,7 @@ def itemViewCarousel(request):
     else:
         return render(request, 'calisphere/carousel.html', {
             'start': queryParams['start'],
-            'search_results': carousel_solr_search.results,
+            'search_results': search_results,
             'item_id': item_id
         })
 
