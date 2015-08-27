f8f1aea7e0e91615aadbd7dbb99bd2562066d66d
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2015-08-27 08:06:33 -0700

    switch to django-contact-form

--- public_interface/urls.py
+++ public_interface/urls.py
@@ -4,6 +4,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('calisphere.urls', namespace="calisphere")),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^contact/', include('contact_form.urls')),
 )
