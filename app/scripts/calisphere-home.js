1aa28aba96c944421ad6c09edeb5d778bd6de111
Amy Wieliczka <amywieliczka@gmail.com>
2015-08-27 17:22:35 -0700

    clear session storage whenever the user navigates to the home page

--- app/scripts/calisphere-home.js
+++ app/scripts/calisphere-home.js
@@ -1,8 +1,14 @@
+/*global _*/
 'use strict';
+
+sessionStorage.clear();
 
 $(document).ready(function() {
   $('html').removeClass('no-jquery');
-  
+  _.each($('form'), function(el) {
+    el.reset();
+  });
+    
   $(document).on('submit', '#js-searchForm, #js-homeForm, #js-footerSearch', function() {
     sessionStorage.setItem('q', $(this).find('input').val());
   });
