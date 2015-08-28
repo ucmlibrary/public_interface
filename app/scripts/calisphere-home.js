97a9b88bd0ed5cfbf2e5ebcce371407213bd35a5
Amy Wieliczka <amywieliczka@gmail.com>
2015-08-28 11:26:42 -0700

    Got the hamburger working on the homepage

--- app/scripts/calisphere-home.js
+++ app/scripts/calisphere-home.js
@@ -8,8 +8,19 @@ $(document).ready(function() {
   _.each($('form'), function(el) {
     el.reset();
   });
-    
+
   $(document).on('submit', '#js-searchForm, #js-homeForm, #js-footerSearch', function() {
     sessionStorage.setItem('q', $(this).find('input').val());
+  });
+
+  $(document).on('click', '.js-global-header__bars-icon', function() {
+    $('.js-global-header__search').toggleClass('global-header__search global-header__search--selected');
+    $('.js-global-header__mobile-links').toggleClass('.global-header__mobile-links global-header__mobile-links--selected');
+  });
+
+  // Toggle only search box:
+  //  $('.js-global-header__search-icon').click(function(){
+  $(document).on('click', '.js-global-header__search-icon', function() {
+    $('.js-global-header__search').toggleClass('global-header__search global-header__search--selected');
   });
 });
