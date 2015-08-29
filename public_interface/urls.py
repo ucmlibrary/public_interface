13b74491f932ccdfc9806e28a4b6c58619c89152
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2015-08-29 13:17:08 -0700

    add a robots.txt
    http://fredericiana.com/2010/06/09/three-ways-to-add-a-robots-txt-to-your-django-project/

--- public_interface/urls.py
+++ public_interface/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 from calisphere.contact_form_view import CalisphereContactFormView
@@ -16,4 +17,7 @@ urlpatterns = patterns('',
         TemplateView.as_view(
             template_name='contact_form/contact_form_sent.html'),
         name='contact_form_sent'),
+    url(r'^robots.txt$',
+        lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")
+    )
 )
