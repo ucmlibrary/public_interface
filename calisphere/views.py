20be96aa7d09359754df244b9dafe7f3496d761c
Amy Wieliczka <amywieliczka@gmail.com>
2015-08-31 15:54:10 -0700

    Fixed bug for when there is no campus context

--- calisphere/views.py
+++ calisphere/views.py
@@ -517,7 +517,7 @@ def itemViewCarousel(request):
             'item_id': item_id,
             'referral': request.GET['referral'] if 'referral' in request.GET else '',
             'referralName': request.GET['referralName'] if 'referralName' in request.GET else '',
-            'campus_slug': request.GET['campus_slug'],
+            'campus_slug': request.GET['campus_slug'] if 'campus_slug' in request.GET else '',
             'linkBackId': linkBackId
         })
     else:
