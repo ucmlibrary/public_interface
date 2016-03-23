3e207e0c2e966a77dd832a22703e74528fb2dc90
Amy Wieliczka <amywieliczka@gmail.com>
2016-03-22 18:00:49 -0700

    Added a url for the page for job opportunities

--- calisphere/urls.py
+++ calisphere/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     url(r'siteMap/$', TemplateView.as_view(template_name='calisphere/siteMap.html'), name='siteMap'),
     url(r'outreach/$', TemplateView.as_view(template_name='calisphere/outreach.html'), name='siteMap'),
     url(r'contribute/$', TemplateView.as_view(template_name='calisphere/contribute.html'), name='siteMap'),
+    url(r'jobs/$', TemplateView.as_view(template_name='calisphere/jobs.html'), name='jobs'),
 
     # AJAX HELPERS
     url(r'^relatedCollections/', views.relatedCollections, name='relatedCollections'),
