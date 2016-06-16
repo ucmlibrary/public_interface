1b68bb00132ee8c561d406023e709b85c36c19f8
Amy Wieliczka <amywieliczka@gmail.com>
2016-06-16 13:22:13 -0700

    Redirected collections/themed/ collections/cal-cultures/ and collections/jarda/ to new exhibition targets. Removed associated templates, views, and buttons.

--- exhibits/urls.py
+++ exhibits/urls.py
@@ -6,8 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.exhibitRandom, name='randomExplore'),
     url(r'jarda-related-resources/$', TemplateView.as_view(template_name='exhibits/jarda-related-resources.html'), name='jarda-related-resources'),
-    # url(r'^special-topics/$', views.themeDirectory, name='themeDirectory'),
-    # url(r'^a-z/$', views.exhibitDirectory, name='exhibitDirectory'),
     url(r'^search/', views.exhibitSearch, name='exhibitSearch'),
     url(r'^browse/(?P<category>[-\w]+)/$', views.exhibitDirectory, name='exhibitDirectory'),
     url(r'^(?P<exhibit_id>\d+)/(?P<exhibit_slug>[-\w]+)/$', views.exhibitView, name='exhibitView'),
